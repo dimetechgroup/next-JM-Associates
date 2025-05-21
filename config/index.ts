6c85fc7ba3e7d2a58531d724373603c375febd74
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-05-21 13:00:48 +0300

    changed token

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ export const MAPS_URL = "https://maps.app.goo.gl/yn4yudwmp3Krs33p8";
 export const MAPS_EMBED =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d249.3029080354238!2d36.80691775698188!3d-1.2646096612091042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f176afe68fe1b%3A0xf590c6d95ac4594f!2sWestpark%20Towers!5e0!3m2!1sen!2ske!4v1743507292234!5m2!1sen!2ske";
 
-  export const CMS_TOKEN = process.env.NEXT_PUBLIC_CMS_TOKEN || "USR-9eddabb30df228c9838467f955102ce2de39535f"
+  export const CMS_TOKEN = process.env.NEXT_PUBLIC_CMS_TOKEN || "API-b62c4dbd0e1b5006fcd6849a848d245c2a2433fa"
